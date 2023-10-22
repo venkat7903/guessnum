f6487c17e97f3e3dd1bbed7481541fad89b8aa21
Venkata Basavayya <138805603+venkat7903@users.noreply.github.com>
2023-10-22 23:18:31 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,29 @@
+const responseEl = document.getElementById("response");
+let randomNum = Math.ceil(Math.random() * 10);
+let spinner = document.getElementById("spinner");
+
+const guessNum = function(event) {
+    spinner.classList.toggle("d-none");
+    responseEl.classList.toggle("d-none");
+    if (parseInt(event.target.value) === randomNum) {
+        setTimeout(() => {
+            responseEl.classList.remove("failed");
+            responseEl.classList.toggle("d-none");
+            spinner.classList.toggle("d-none");
+            responseEl.textContent = "💥Correct Guess💥";
+            responseEl.classList.add("success");
+        }, 1000);
+        randomNum = Math.ceil(Math.random() * 10);
+        console.log(randomNum);
+    } else {
+        setTimeout(() => {
+            responseEl.classList.remove("success");
+            responseEl.classList.toggle("d-none");
+            spinner.classList.toggle("d-none");
+            responseEl.textContent = "🤞Better Luck next time🤞";
+            responseEl.classList.add("failed");
+        }, 1000);
+    }
+};
+
+console.log(randomNum);
